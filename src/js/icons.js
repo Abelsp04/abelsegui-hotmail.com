49e73676955c13c287bb92b69052f4404872a4c2
Abel Segui <Abelsegui@hotmail.com>
2020-03-30 21:25:52 +0000

    first comit

---
+++ src/js/icons.js
@@ -0,0 +1,7 @@
+import fontawesome from "@fontawesome/fontawesome";
+import faClock from "@fontawesome/fontawesome-free-regular/faClock";
+
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faClock);
