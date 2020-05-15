c557f9b90a7c2b289244d4040671f2c4c28d3541
Abel Segui <Abelsegui@hotmail.com>
2020-05-15 03:59:43 +0000

    345

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,12 @@ import "../styles/index.scss";
 function SimpleCounter(props) {
 	return (
 		<div className="bigCounter">
+			<nav className="navbar navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					<img src="/docs/4.5/assets/brand/bootstrap-solid.svg" />
+					Bootstrap
+				</a>
+			</nav>
 			<div className="calendar">
 				<i className="far fa-clock" />
 			</div>
